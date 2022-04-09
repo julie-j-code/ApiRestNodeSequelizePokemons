05812ab7fd12c4099b7510e7c7bd0ada5d04c5ed
Julie J <jeannet.julie@gmail.com>
2022-04-09 07:28:36 +0200

    Ajout instruction return dans updatePokemon pour eviter la duplication du code erreur 500

--- src/routes/updatePokemon.js
+++ src/routes/updatePokemon.js
@@ -7,17 +7,19 @@ module.exports = (app) => {
       where: { id: id }
     })
       .then(_ => {
-        Pokemon.findByPk(id).then(pokemon => {
+        // le fait de rajouter l'instrucrion return va permettre de factoriser la gestion de l'erreur 500
+        // et donc de supprimer la duplication de code concernant cette erreur
+        return Pokemon.findByPk(id).then(pokemon => {
           if (pokemon === null) {
             const message = `Le pokémon demandé n'existe pas. Réessayez avec un autre identifiant.`
             return res.status(404).json({ message })
           }
           const message = `Le pokémon ${pokemon.name} a bien été modifié.`
           res.json({ message, data: pokemon })
-            .catch(error => {
-              const message = `Le  pokémon n'a pas pu être modifié. Réessayer dans quelques instants`
-              res.status(500).json({ message, data: error })
-            })
+            // .catch(error => {
+            //   const message = `Le  pokémon n'a pas pu être modifié. Réessayer dans quelques instants`
+            //   res.status(500).json({ message, data: error })
+            // })
         })
           .catch(error => {
             const message = `Le  pokémon n'a pas pu être modifié. Réessayer dans quelques instants`
