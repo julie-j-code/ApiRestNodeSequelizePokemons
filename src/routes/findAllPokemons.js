2559b9ce2c043bc23e197ffa158d014d074aece4
Julie J <jeannet.julie@gmail.com>
2022-04-10 16:04:23 +0200

    Cas de paramètres de requête simple basé sur le nom where : {name:name}})

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -6,15 +6,27 @@ const { Pokemon } = require('../db/sequelize')
 module.exports = (app) => {
   app.get('/api/pokemons', (req, res) => {
     //   retourne une promesse contenant la liste de pokemons présents dans la base de donnée
-    Pokemon.findAll()
+    
+    // exemple de paramètres de requête personnelle
+    if (req.query.name) {      
+      const name=req.query.name;
+      return Pokemon.findAll({where : {name:name}})
       .then(pokemons => {
-        const message = 'La liste des pokémons a bien été récupérée.'
-        res.json({ message, data: pokemons })
-        // puis on va utiliser la méthode catch() de javascript
+        const message = `Il y a ${pokemons.length} pokémons qui correspondent à la recherches.`
+        res.json({message, data:pokemons})
       })
-      .catch(error=>{
-        const message = `La liste des erreurs n'a pas  pu être récupérée, réessayez dans quelques instants`
-        res.status(500).json({message, data:error})
-      })
+    }
+    else{
+      Pokemon.findAll()
+        .then(pokemons => {
+          const message = 'La liste des pokémons a bien été récupérée.'
+          res.json({ message, data: pokemons })
+          // puis on va utiliser la méthode catch() de javascript
+        })
+        .catch(error=>{
+          const message = `La liste des erreurs n'a pas  pu être récupérée, réessayez dans quelques instants`
+          res.status(500).json({message, data:error})
+        })
+    }
   })
 }
