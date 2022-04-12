dacb19027b13e1cb006bc270f8765a854c07b13b
Julie J <jeannet.julie@gmail.com>
2022-04-12 16:50:27 +0200

    Modification sequelize.js pour paramétrer deux connexions différentes selon l'environnement heroku ou local

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -4,14 +4,24 @@ const PokemonModel = require('../models/pokemon')
 const UserModel = require('../models/user')
 const pokemons = require('./mock-pokemon')
 
-const sequelize = new Sequelize('pokedex', 'root', '', {
+if(process.env.NODE_ENV === 'production') {
+  sequelize = new Sequelize('h4emme7k3w50fawh', 'wzn2i59vbv85be0z', 'uu7p8lb06yvt0jt3', {
+    host: 'uyu7j8yohcwo35j3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    dialect: 'mariadb',
+    dialectOptions: {
+      timezone: 'Etc/GMT-2',
+    },
+    logging: true
+  })
+} else{
+  sequelize = new Sequelize('pokedex', 'root', '', {
   host: 'localhost',
   dialect: 'mariadb',
   dialectOptions: {
     timezone: 'Etc/GMT-2',
   },
   logging: false
-})
+})}
 
 const Pokemon = PokemonModel(sequelize, DataTypes)
 // on instancie le modèle User au près de Sequelize
